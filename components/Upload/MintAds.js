1af9bc42a3aba87438a00f3b7e44e9d5764d4a44
Harsh Ghodkar <harshghodkar@gmail.com>
2022-03-08 23:35:26 +0530

    updates

--- components/Upload/MintAds.js
+++ components/Upload/MintAds.js
@@ -2,6 +2,15 @@ import { useMoralis } from "react-moralis";
 import { Fragment, useEffect, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+import { actions, NodeWallet } from "@metaplex/js";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import {
+  clusterApiUrl,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
 
 export default function MintVideos() {
   const { Moralis } = useMoralis();
@@ -9,6 +18,24 @@ export default function MintVideos() {
   const [selected, setSelected] = useState();
   const [categories, setCategories] = useState([]);
   const [selectedId, setSelectedId] = useState(new Map());
+
+  const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
+    "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+  );
+
+  let associatedAddress = "";
+
+  async function findAssociatedTokenAddress(walletAddress, tokenMintAddress) {
+    const array = await PublicKey.findProgramAddress(
+      [
+        walletAddress.toBuffer(),
+        TOKEN_PROGRAM_ID.toBuffer(),
+        tokenMintAddress.toBuffer(),
+      ],
+      SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+    );
+    associatedAddress = array[0];
+  }
 
   useEffect(() => {
     const ChannelCategory = Moralis.Object.extend("ChannelCategory");
@@ -26,25 +53,98 @@ export default function MintVideos() {
   }, []);
 
   async function createAd(e) {
+    // Solana stuff
+
+    const keypair = Keypair.generate();
+    console.log("keypair", keypair);
+    const wallet = new NodeWallet(keypair);
+    let user = Moralis.User.current();
+    let userWallet = new PublicKey(user.get("solAddress"));
+    console.log("userWallet", userWallet);
+    //
     e.preventDefault();
     const adTitle = document.getElementById("adTitle").value;
     const adDescription = document.getElementById("adDescription").value;
     const adLink = document.getElementById("link").value;
     const selectedCategory = selected;
 
+    // Store the IMAGE on IPFS and pass the URI here
+    const imageURI = "";
+
     const metadata = {
       name: adTitle,
+      symbol: "SOL",
       description: adDescription,
       hyperlink: adLink,
       category: selectedCategory,
+      image: imageURI,
+      seller_fee_basis_points: 0,
+      properties: {
+        files: [
+          {
+            uri: imageURI,
+            type: "image/jpeg",
+          },
+        ],
+        category: "image",
+        creators: [
+          {
+            address: keypair.publicKey,
+            verified: true,
+            share: 100,
+          },
+        ],
+      },
     };
 
     const metadataFile = new Moralis.File("metadata.json", {
       base64: btoa(JSON.stringify(metadata)),
     });
 
-    await metadataFile.saveIPFS();
+    await metadataFile
+      .saveIPFS()
+      .then(console.log("Metadata file saved to IPFS"));
     const metadataURI = metadataFile.ipfs();
+
+    // minting
+    console.log("Minting");
+
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    console.log("connection", connection);
+
+    const feePayerAirdropSignature = await connection.requestAirdrop(
+      keypair.publicKey,
+      LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(feePayerAirdropSignature);
+    let mintaddress = "";
+    const mintNFTResponse = await actions
+      .mintNFT({
+        connection,
+        wallet: wallet,
+        uri: metadataURI,
+        maxSupply: 1,
+      })
+      .then((mintNFTResponse) => {
+        mintaddress = mintNFTResponse.mint;
+      });
+
+    await findAssociatedTokenAddress(keypair.publicKey, mintaddress);
+    const sendingToken = await actions
+      .sendToken({
+        connection,
+        amount: 1,
+        destination: userWallet,
+        source: associatedAddress,
+        wallet: wallet,
+        mint: mintaddress,
+      })
+      .then((response) => {
+        console.log("response", response);
+        alert("Ad created");
+      });
+
+    ////////////////////////////
 
     const AdContent = new Moralis.Object.extend("AdContent");
     const adContent = new AdContent();
