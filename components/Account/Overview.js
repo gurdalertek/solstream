2a648ad9ea46f8b5fec88b804afc0cddb603196f
Felix <dexdaprotocol@gmail.com>
2022-03-15 14:17:05 -0400

    updates

--- components/Account/Overview.js
+++ components/Account/Overview.js
@@ -141,18 +141,6 @@ export default function Account() {
     }
   }
 
-  // useEffect(() => {
-  //   const AdContent = Moralis.Object.extend("AdContent");
-  //   const query = new Moralis.Query(AdContent);
-  //   const votes = new AdContent();
-  //   query.find().then(function (results) {
-  //     results.forEach((result) => {
-  //       console.log(result);
-  //     });
-  //   });
-  //   setVotes(votes);
-  // }, []);
-
   return (
     <div className="mt-8 flex flex-col items-center justify-center">
       <h1 className="flex text-xl font-bold mb-8">ACCOUNT</h1>
@@ -160,7 +148,6 @@ export default function Account() {
         onClick={() => navigator.clipboard.writeText(userAddress)}
         className="cursor-pointer hover:underline flex active:text-[#9945FF]"
       >
-        {/* {userAddress.slice(0, 8).concat("...")} */}
         {userAddress}
       </div>
       <div className="flex flex-row items-center w-6/12 justify-evenly mt-8">
@@ -182,15 +169,6 @@ export default function Account() {
           Mint Gov Token
         </button>
       </div>
-      {/* <div className="mt-8 flex flex-row">
-        <button
-          className={`border-2 border-[#14F195] flex flex-row items-center p-2 m-4 rounded-lg whitespace-nowrap`}
-          onClick={govSwitch}
-        >
-          {!isGovernor ? "Votes" : "Governance Token"}
-          <SwitchHorizontalIcon className="h-3 ml-2" />
-        </button>
-      </div> */}
       {isGovernor ? (
         <div className="bg-black bg-opacity-25 mt-8 sm:w-6/12 w-full flex flex-col items-center justify-center mb-8 rounded-xl">
           <div className="flex w-full flex-col items-center justify-center my-8">
